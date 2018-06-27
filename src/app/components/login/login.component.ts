0bc9f027172782a3ad666ec803952f4d4c42542a
BloodyLink <david.quezada.m@gmail.com>
2018-06-27 00:25:18 -0400

    testing

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,132 @@
-import { Component, OnInit } from '@angular/core';
-import { LoginService, UsersService } from '../../services/index';
+import { Component, Inject } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+  PatternValidator,
+  EmailValidator
+} from "@angular/forms";
+import { LoginService, UsersService } from "../../services/index";
+// import * as M from "materialize-css/dist/js/materialize";
+import { MAX_LENGTH_VALIDATOR } from "@angular/forms/src/directives/validators";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "login",
+  templateUrl: "login.component.html",
+  styleUrls: ["login.component.scss"]
 })
-export class LoginComponent implements OnInit {
-  constructor(private usersService: UsersService) {}
+export class LoginComponent {
+  loginForm: FormGroup;
+  updateForm: FormGroup;
+  resetForm: FormGroup;
+  resetPasswordDiv = false;
+  resetPassword = true;
+  loginDiv = false;
+  loading = false;
+  returnUrl: string;
+  invalidCredentials;
+  errorMessage = "";
+  gifPath = "";
+  updatePasswordDiv = false;
+  updatPassword = true;
 
-  ngOnInit(): void {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    // private giphyService: GiphyService,
+    private loginService: LoginService,
+    private userService: UsersService,
+    @Inject(FormBuilder) private fb: FormBuilder
+  ) {
+    this.loginFormValid();
+    this.resetFormValid();
+    this.getGif();
+  }
+  loginFormValid() {
+    this.loginForm = this.fb.group({
+      email: [
+        null,
+        [Validators.required, Validators.email, Validators.maxLength(90)]
+      ],
+      password: [null, [Validators.required, Validators.maxLength(90)]]
+    });
+  }
+  resetFormValid() {
+    this.resetForm = this.fb.group({
+      email: [
+        null,
+        [Validators.required, Validators.email, Validators.maxLength(90)]
+      ]
+    });
+  }
+  logIn() {
+    if (this.loginForm.valid) {
+      this.invalidCredentials = false;
+      this.loading = true;
+      this.loginService
+        .login(
+          this.loginForm.get("email").value,
+          this.loginForm.get("password").value
+        )
+        .subscribe(
+          result => {
+            this.router.navigate([this.returnUrl]);
+            if (result.state === "OK") {
+              console.log("Yay! You're a rockstar :D");
+            } else {
+              console.log("Ops! Something was wrong :c");
+            }
+          },
+          error => {
+            /* this.alertService.error(error); */
+            this.loading = false;
+            console.log("Ops! There is something wrong :c");
+            this.invalidCredentials = true;
+          }
+        );
+    } else {
+      this.invalidCredentials = true;
+    }
+  }
+  resetPass() {
+    this.resetPasswordDiv = true;
+    this.loginDiv = true;
+    this.invalidCredentials = false;
+  }
+  sendPassword() {
+    console.log("error");
+    if (this.resetForm.valid) {
+      this.userService.getByEmail(this.resetForm.get("email").value).subscribe(
+        result => {
+          console.log("This does exist");
+          this.resetPassword = false;
+        },
+        error => {
+          this.invalidCredentials = true;
+          console.log("This doesn't exist");
+        }
+      );
+    } else {
+      this.invalidCredentials = true;
+    }
+  }
+  ngOnInit() {
+    this.loginService.logout();
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/";
+  }
+  //Giphy logic
+  getGif(limit = 10) {
+    this.giphyService.getGif("success", limit).subscribe(data => {
+      const randomOffset: number = Math.floor(Math.random() * limit + 0);
+      this.gifPath = data.data[randomOffset].images.fixed_height.url;
+    }, error => (this.errorMessage = <any>error));
+  }
+  showRecovery() {
+    console.log("click");
+    this.updatePasswordDiv = true;
+    console.log(this.updatePasswordDiv);
+  }
 }
